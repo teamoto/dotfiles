30a7cdd13dce2dfc01a6775c54b35febe1ca6598
Tatsuya <tatsuya071@gmail.com>
2024-10-17 07:57:17 -0400

    Added feature to configure vscode for tumbleweed and debian

--- configre_dotfiles.sh
+++ configre_dotfiles.sh
@@ -7,39 +7,39 @@ set -euo pipefail
 BACKUP_RETENTION=7
 ## Should you need to use any tmux plugins, ensure to set the TMUX_PLUGIN_MANAGER variable.
 TMUX_PLUGIN_MANAGER="tmux-plugins/tpm"
+## Should you need to use any vscode extensions, ensure to set the VSCODE_EXTENSIONS variable.
+VSCODE_EXTENSIONS=(
+    "donjayamanne.githistory"
+    "editorconfig.editorconfig"
+    "esbenp.prettier-vscode"
+    "github.copilot"
+    "github.copilot-chat"
+    "gruntfuggly.todo-tree"
+    "hediet.vscode-drawio"
+    "mads-hartmann.bash-ide-vscode"
+    "ms-python.debugpy"
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+    "oderwat.indent-rainbow"
+    "rogalmic.bash-debug"
+    "shd101wyy.markdown-preview-enhanced"
+    "streetsidesoftware.code-spell-checker"
+    "teabyii.ayu"
+    "usernamehw.errorlens"
+    "vscode-icons-team.vscode-icons"
+    "wayou.vscode-todo-highlight"
+    "yzhang.markdown-all-in-one"
+)
+
 
 ## Do not change these variables.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES_DIR="${HOME}/.dotfiles"
-DEFAULT_COLOR="\033[0m"
-YELLOW_COLOR="\033[1;33m"
-RED_COLOR="\033[1;31m"
-
 
 
 # Functions
-function time_echo() {
-    # Print a message with a timestamp.
-    local message="$1"
-    local date_time=$(date +'%Y-%m-%d %H:%M:%S')
-    local color="$2"
-    echo -e "${color}${date_time} - ${message}${DEFAULT_COLOR}"
-}
-
-function info() {
-    local message="$1"
-    time_echo "INFO: ${message}" "${DEFAULT_COLOR}"
-}
-
-function warn() {
-    local message="$1"
-    time_echo "WARN: ${message}" "${YELLOW_COLOR}"
-}
-
-function error() {
-    local message="$1"
-    time_echo "ERROR: ${message}" "${RED_COLOR}"
-}
+# Import the base functions
+source "${SCRIPT_DIR}/utils/base_functions.sh"
 
 function backup_dotfiles() {
     # Backup the existing dotfiles.
@@ -63,16 +63,6 @@ function backup_dotfiles() {
             sort | head -n "${excess_backups}" | cut -d' ' -f2- | xargs -I {} rm -rf {}
     fi
 
-}
-
-function is_installed() {
-    # Check if a command is installed.
-    local command="$1"
-    if command -v "${command}" &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
 }
 
 # Vim
@@ -101,7 +91,7 @@ function install_tmux_plugins() {
             git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         fi
     fi
-    
+
 }
 
 ## Tmux configuration
@@ -120,7 +110,93 @@ function configure_tmux() {
     tmux source "${HOME}/.tmux.conf"
 }
 
-# Main function to configure the dotfiles.
+# vscode
+
+# ToDo:
+# - Add a function to detect the OS.
+# - Add functions and shortcut keys tailored for MacOS.
+
+
+## Install extensions
+function install_vscode_extension() {
+    local extension="$1"
+    if [[ -z "${extension}" ]]; then
+        error "No extension provided."
+        return 1
+    fi
+    info "Installing the ${extension} extension..."
+    if grep -q "${extension}" <<<$(code --list-extensions); then
+        warn "The ${extension} extension is already installed. Skipping..."
+    else
+        if code --install-extension "${extension}"; then
+            info "The ${extension} extension was successfully installed."
+        else
+            warn "Failed to install the ${extension} extension."
+        fi
+    fi
+    return 0
+}
+
+## Backup the current vscode settings and keybindings
+function backup_vscode_setting() {
+    local user_config_dir="${HOME}/.config/Code/User"
+    local backup_dir="${HOME}/.config/Code/User/backup"
+
+    if [[ ! -d "${backup_dir}" ]]; then
+        info "Creating the vscode user configuration backup directory..."
+        mkdir -p "${backup_dir}"
+    fi
+
+    info "Backing up the vscode user configuration..."
+    if [[ -f "${user_config_dir}/settings.json" ]]; then
+        info "Backing up the vscode settings..."
+        cp -f "${user_config_dir}/settings.json" "${backup_dir}/settings.json"
+    else
+        warn "The vscode settings file does not exist."
+        return 1
+    fi
+    info "Backing up the vscode keybindings..."
+    if [[ -f "${user_config_dir}/keybindings.json" ]]; then
+        cp -f "${user_config_dir}/keybindings.json" "${backup_dir}/keybindings.json"
+    else
+        warn "The vscode keybindings file does not exist."
+        return 1
+    fi
+    return 0
+}
+
+## Configure vscode settings and keybindings
+function configure_vscode() {
+    local user_config_dir="${HOME}/.config/Code/User"
+
+    info "Checking if VSCode is installed..."
+    if ! is_installed "code"; then
+        warn "VSCode is not installed."
+        return 0
+    fi
+
+    if [[ ! -d "${user_config_dir}" ]]; then
+        info "Creating the vscode user configuration directory..."
+        mkdir -p "${user_config_dir}"
+    fi
+
+    backup_vscode_setting
+
+    info "Overwriting the vscode settings..."
+    cp -f "${DOTFILES_DIR}/vscode/settings.json" "${user_config_dir}/settings.json"
+    info "Overwriting the vscode keybindings..."
+    cp -f "${DOTFILES_DIR}/vscode/keybindings.json" "${user_config_dir}/keybindings.json"
+
+    info "Installing the vscode extensions..."
+    for extension in "${VSCODE_EXTENSIONS[@]}"; do
+        install_vscode_extension "${extension}"
+    done
+
+}
+
+
+# Main
+## Main function to configure the dotfiles.
 function main() {
     # Create the dotfiles directory if it does not exist.
     info "Creating the dotfiles directory..."
@@ -141,9 +217,11 @@ function main() {
     cp -r "${SCRIPT_DIR}/dotfiles/." "${DOTFILES_DIR}/"
 
     # Create symbolic links to the dotfiles.
-    
+
     configure_vim
     configure_tmux
+    configure_vscode
+
 
     info "Dotfiles configuration successfully complated."
 }
---
+++ utils/configure_vscode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Environment variables
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Functions
+function 
